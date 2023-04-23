07fe38f7d49313effc5974f14629bef486ef9cd0
javpintos <jpintos234@gmail.com>
2023-04-23 18:34:28 -0300

    clase Ronda y dependencias agregadas

---
+++ src/main/java/ar/utn/ap/pronosticos/Ronda.java
@@ -0,0 +1,32 @@
+package ar.utn.ap.pronosticos;
+
+public class Ronda {
+    private String nro;
+    private Partido[] partidos;
+
+    public Ronda(String nro, Partido[] partidos) {
+        this.nro = nro;
+        this.partidos = partidos;
+    }
+
+    public String getNro() {
+        return nro;
+    }
+
+    public void setNro(String nro) {
+        this.nro = nro;
+    }
+
+    public Partido[] getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(Partido[] partidos) {
+        this.partidos = partidos;
+    }
+
+    public int puntos(){
+        //ToDo
+        return 0;
+    }
+}
